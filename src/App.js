310161c9897a28762561b9a7e410d3098e7431b3
stephy <stephshiro35@gmail.com>
2024-05-09 00:14:20 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Home from './components/Home';
-import Page from './components/Page';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Home from "./components/Home";
+import Page from "./components/Page";
+import { Link } from "react-router-dom";
+
+function generateRandomCharacter() {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    return characters[randomIndex];
+  }
 
 const App = () => {
   const [accessToken, setAccessToken] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(generateRandomCharacter());
   const [searchResults, setSearchResults] = useState([]);
-  const [isHomePage, setIsHomePage] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [isHomePage, setIsHomePage] = useState(true);
+
+  const handleToggle = () => {
+    setIsHomePage(!isHomePage);
+    // Clear input value based on the toggle
+    if (isHomePage) {
+      setSearchTerm(""); // Clear the input value for !isHomePage
+    }
+  };
 
   const getAccessToken = async () => {
     const params = new URLSearchParams();
-    params.append('client_id', 'd4d6a6bb920d40859a149b97feb4795d');
-    params.append('client_secret', 'cc1094f210974810ae180d6ca69a8a2a');
-    params.append('grant_type', 'client_credentials');
+    params.append("client_id", "d4d6a6bb920d40859a149b97feb4795d");
+    params.append("client_secret", "cc1094f210974810ae180d6ca69a8a2a");
+    params.append("grant_type", "client_credentials");
 
     try {
-      const response = await axios.post('https://accounts.spotify.com/api/token', params.toString(), {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        params.toString(),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
       setAccessToken(response.data.access_token);
     } catch (error) {
-      console.error('Error getting access token:', error);
+      console.error("Error getting access token:", error);
     }
   };
 
   const fetchSearchResults = async () => {
     try {
-      const response = await axios.get('https://api.spotify.com/v1/search', {
+      const response = await axios.get("https://api.spotify.com/v1/search", {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
         params: {
           q: searchTerm,
-          type: 'track,artist,album,playlist',
+          type: "track,artist,album,playlist",
         },
       });
       setSearchResults(response.data);
+      console.log (response.data);
     } catch (error) {
-      console.error('Error fetching search results:', error);
+      console.error("Error fetching search results:", error);
     }
   };
 
@@ -49,54 +69,108 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    if (accessToken && searchTerm !== '') {
+    if (accessToken) {
       fetchSearchResults();
     }
   }, [accessToken, searchTerm]);
-
   return (
     <div className="flex flex-col h-screen">
       <nav className="bg-gray-800 p-4 flex justify-between items-center">
         <h1 className="text-white text-xl">Tune-Stream</h1>
         <button
-          type='button'
-          onClick={() => setIsHomePage(!isHomePage)}
-          className="size-12  border-2 border-gray-300 rounded-full flex items-center justify-center text-white"
+          type="button"
+          /* onClick={() => setIsHomePage(!isHomePage)} */
+          onClick={handleToggle}
+          className="size-12 border-2 border-gray-300 rounded-full flex items-center justify-center text-white"
         >
           {isHomePage ? (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 text-white"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
               <path d="M21 10l-9.7-9.7a.7.7 0 00-.982 0L3 10M21 10l-9.7 9.7a.7.7 0 01-.982 0L3 10M21 10H3" />
             </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 3a1 1 0 011 1v2.293l3.646-3.647a1 1 0 111.415 1.414L11.414 8l3.647 3.647a1 1 0 11-1.414 1.414L10 9.415V12a1 1 0 11-2 0V6a1 1 0 011-1z" clipRule="evenodd" />
-              <path fillRule="evenodd" d="M3 10a1 1 0 011-1h2.293L3.646 5.354a1 1 0 10-1.414 1.414L6.586 9 2.232 13.354a1 1 0 101.414 1.414L6 10.414V13a1 1 0 102 0v-6a1 1 0 01-1-1z" clipRule="evenodd" />
-              <path fillRule="evenodd" d="M17 10a1 1 0 00-1-1h-2.293l3.647-3.646a1 1 0 10-1.414-1.414L13.415 9l4.354 4.354a1 1 0 101.414-1.414L15.414 11H18a1 1 0 001-1z" clipRule="evenodd" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 text-white"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 3a1 1 0 011 1v2.293l3.646-3.647a1 1 0 111.415 1.414L11.414 8l3.647 3.647a1 1 0 11-1.414 1.414L10 9.415V12a1 1 0 11-2 0V6a1 1 0 011-1z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M3 10a1 1 0 011-1h2.293L3.646 5.354a1 1 0 10-1.414 1.414L6.586 9 2.232 13.354a1 1 0 101.414 1.414L6 10.414V13a1 1 0 102 0v-6a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M17 10a1 1 0 00-1-1h-2.293l3.647-3.646a1 1 0 10-1.414-1.414L13.415 9l4.354 4.354a1 1 0 101.414-1.414L15.414 11H18a1 1 0 001-1z"
+                clipRule="evenodd"
+              />
             </svg>
           )}
         </button>
 
-        {/* Search Input */}
+        {/* Search Input*/}
         <div className="flex-1 flex justify-center">
           <div className="grid place-items-center">
-            <input
-              type="text"
-              placeholder='Search...'
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)} // Update searchTerm on input change
-              className="w-96 p-4 border border-gray-300 rounded-lg"
-            />
+            {isHomePage && (
+              <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-96 p-4 border border-gray-300 rounded-lg"
+                style={{ display: "none" }}
+              />
+            )}
+            {!isHomePage && (
+                <input
+                type="text"
+                placeholder="Search..."
+                defaultValue={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-96 p-4 border border-gray-300 rounded-lg"
+                />
+            )}
           </div>
         </div>
 
         <div className="flex items-center">
-          <button className="text-white mr-4">Login</button>
-          <button className="text-white">Sign Up</button>
+          <Link
+            to="/login"
+            className="bg-blue-400 text-slate-50 hover:bg-blue-300/90 py-1 px-2 mr-4"
+            style={{ borderRadius: "0.5rem" }}
+          >
+            Login
+          </Link>
+          <Link
+            to="/signup"
+            className="bg-blue-400 text-slate-50 hover:bg-blue-300/90 py-1 px-2"
+            style={{ borderRadius: "0.5rem" }}
+          >
+            Sign Up
+          </Link>
         </div>
       </nav>
 
       <div className="flex-1 bg-gray-100">
-        {isHomePage ? <Home /> : <Page searchTerm={searchTerm} />} {/* Pass searchTerm to Page component */}
+        {isHomePage ? (
+          <Home searchResults={searchResults} />
+        ) : (
+          <Page searchResults={searchResults} />
+        )}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 
 
+import LoginPage from './components/LoginPage';
+
+import SignUpPage from './components/SignUpPage';
+// import Footer from './components/footer';
+// import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  // {
+  //   path: "/products",
+  //   element: <ProductList />
+  // },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  // {
+  //   path: "/contact",
+  //   element: <ContactForm />
+  // },
+  //{
+  //   path: "/feedback",
+  //   element: <FeedbackForm />
+  // },
+  {
+    path: "/signup",
+    element: <SignUpPage />
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render
+root.render(<RouterProvider router={router} />);
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
